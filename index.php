2f519f4a4f71508d9290676b6b50d470b6c7c32c
Dylan Galindo <xploredroidx@gmail.com>
2018-11-29 13:00:20 +0100

    Primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo '<h1>Proyecto para gitHub</h1>';
+            echo "<p>Nueva modificacion apartado 2</p>";
+        ?>
+    </body>
+</html>
