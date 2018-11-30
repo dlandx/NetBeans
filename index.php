3635fc1f2008234ea28ebddda1ed951c0a1a8f49
dlandx <45194345+dlandx@users.noreply.github.com>
2018-11-30 13:34:38 +0100

    Modificamos desde GitHub

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
             echo '<h1>Proyecto para gitHub</h1>';
             echo "<p>Nueva modificacion apartado 2</p>";
             echo "<h2>Otra modificacion para comparar</h2>";
+        
+            echo "<p>Modificacion desde GitHub</p>"
         ?>
     </body>
 </html>
